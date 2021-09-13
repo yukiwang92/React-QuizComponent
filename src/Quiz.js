366771b61b4a451aaf3c6b7f085631c205b2685b
Yuki Wang <moonyw0730@gmail.com>
2021-09-13 23:35:38 +1000

    Save quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends React.Component{
     render() {
         return (
             <div>
-                <div className="QuizQuestion">
+                <div className='QuizQuestion'>
                     {quizData.quiz_questions[0].instruction_text}
                 </div>
             </div>
