483fa1276a04a8a0f6899067f449b755d85985e5
Yuki Wang <moonyw0730@gmail.com>
2021-09-14 00:08:05 +1000

    Creating a quizquestion component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,6 @@
+import QuizQuestion from './QuizQuestion.js';
 import { render } from "enzyme";
 import React, { Component } from "react"
-
 let quizData = require("./quiz_data.json")
 
 class Quiz extends React.Component{
@@ -12,7 +12,7 @@ class Quiz extends React.Component{
         return (
             <div>
                 <div className='QuizQuestion'>
-                    {quizData.quiz_questions[0].instruction_text}
+                    <QuizQuestion quiz_question={this.state.quiz_position.quizData.quiz_question - 1}/>
                 </div>
             </div>
         )
---
+++ src/QuizQuestion.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+class QuizQuestion extends React.Component{
+    constructor(props){
+        super(props)
+    }
+render(){
+    return (
+        <main>
+            <section>
+                <p>{this.props.instruction_text}</p>
+            </section>
+            <section className="buttons">
+                <ul>
+                    <li>{this.props.quiz_question.anwser_options[0]}</li>
+                </ul>
+            </section>
+        </main>
+    );
+}
+}
+export default QuizQuestion
